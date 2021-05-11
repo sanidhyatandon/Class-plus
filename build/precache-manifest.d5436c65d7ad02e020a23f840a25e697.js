50dbf413e403eabe3b9b2d063154fa40e93662bc
Sanidhya Tandon <sanidhya.tandon@gmail.com>
2021-05-12 05:18:40 +0530

    Pused first build

---
+++ build/precache-manifest.d5436c65d7ad02e020a23f840a25e697.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe6fd0695f3c6778f1d26f0e6571d4a6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "79a99ae54c264c82ad2d",
+    "url": "./static/css/main.3288ebc2.chunk.css"
+  },
+  {
+    "revision": "c5df6e6e03af86ef5d9c",
+    "url": "./static/js/2.51ad9041.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.51ad9041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79a99ae54c264c82ad2d",
+    "url": "./static/js/main.beb450f3.chunk.js"
+  },
+  {
+    "revision": "0008ca8bb5decc6c5f84",
+    "url": "./static/js/runtime-main.f840de79.js"
+  }
+]);
